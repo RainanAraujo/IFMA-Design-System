321456f775b6c7e3c3120bc442e17a9c3a1e2203
RainanAraujo <rainanaraaujo@gmail.com>
2024-11-05 10:38:01 -0300

    fix: try deploy

---
+++ storybook-static/assets/Login.stories-DdUWuvCT.js
@@ -0,0 +1,17 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{B as o}from"./Button-CjbFH56n.js";import{I as s}from"./InputField-B48p1609.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Loading-Der4nm1y.js";import"./IconBase-BiRCNEL7.js";import"./cn-DHkRjjEP.js";import"./Input-BfFmV5y9.js";import"./Label-B4RhECvQ.js";const h={title:"Templates/LoginPage",parameters:{layout:"fullscreen"}},l=()=>e.jsx("div",{className:"flex flex-col w-full h-screen p-5",children:e.jsxs("div",{className:"h-full w-full flex flex-col items-center justify-center gap-10",children:[e.jsx("img",{src:"logo",alt:"logo",className:"w-96 h-40 bg-gray-100"}),e.jsxs("div",{className:"max-w-96 w-full  flex flex-col items-center gap-8 ",children:[e.jsx("h1",{className:"text-medium  ",children:"Lorem, ipsum"}),e.jsxs("form",{className:"space-y-3 w-full",children:[e.jsx(s,{label:"Lorem, ipsum",placeholder:"Lorem, ipsum"}),e.jsx(s,{label:"Lorem, ipsum",placeholder:"Lorem, ipsum"}),e.jsx(o,{size:"small",className:"w-full",children:"Lorem, ipsum"})]})]})]})});l.__docgenInfo={description:"",methods:[],displayName:"LoginPage"};var m,r,a;l.parameters={...l.parameters,docs:{...(m=l.parameters)==null?void 0:m.docs,source:{originalSource:`() => {
+  return <div className="flex flex-col w-full h-screen p-5">
+      <div className="h-full w-full flex flex-col items-center justify-center gap-10">
+        <img src="logo" alt="logo" className="w-96 h-40 bg-gray-100" />
+        <div className="max-w-96 w-full  flex flex-col items-center gap-8 ">
+          <h1 className="text-medium  ">Lorem, ipsum</h1>
+          <form className="space-y-3 w-full">
+            <InputField label="Lorem, ipsum" placeholder="Lorem, ipsum" />
+            <InputField label="Lorem, ipsum" placeholder="Lorem, ipsum" />
+            <Button size="small" className="w-full">
+              Lorem, ipsum
+            </Button>
+          </form>
+        </div>
+      </div>
+    </div>;
+}`,...(a=(r=l.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const L=["LoginPage"];export{l as LoginPage,L as __namedExportsOrder,h as default};
