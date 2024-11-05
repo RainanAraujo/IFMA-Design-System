00dd4b3d24c7ab50a0bbca16bbb77b83b50736a7
RainanAraujo <RainanAraujo@users.noreply.github.com>
2024-11-05 22:49:08 +0000

    deploy: bc6dac615d028df410e44e92eaae6fe25ec06793

--- storybook/assets/Loading.stories-CoRSRQM_.js
+++
@@ -1,3 +0,0 @@
-import{j as m}from"./jsx-runtime-j_jdvEMj.js";import{L as t}from"./Loading-Der4nm1y.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./IconBase-BiRCNEL7.js";import"./cn-DHkRjjEP.js";const u={title:"Atoms/Loading",component:t,parameters:{layout:"centered"},argTypes:{className:{control:{disable:!0}}}},r={render:s=>m.jsx(t,{...s,children:"Label"})};var e,a,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  render: args => <Loading {...args}>Label</Loading>
-}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,u as default};
