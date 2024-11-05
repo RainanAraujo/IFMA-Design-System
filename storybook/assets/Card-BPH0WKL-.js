d828b18db5c61fde1628536b0bc0d3d148f64893
RainanAraujo <RainanAraujo@users.noreply.github.com>
2024-11-05 18:30:50 +0000

    deploy: e03e4b92a65a6469cc26fd7c13f14c44ebfbb95a

--- storybook/assets/Card-BPH0WKL-.js
+++
@@ -1,4 +0,0 @@
-import{j as s}from"./jsx-runtime-j_jdvEMj.js";import{c as n}from"./cn-DHkRjjEP.js";const l=({title:r,description:t,cover:e,className:i,...a})=>s.jsxs("div",{className:n("flex flex-col gap-4 items-center w-56 ",i),...a,children:[s.jsx("img",{className:"h-40 w-full bg-gray-100 cursor-pointer",src:e==null?void 0:e.src,alt:e==null?void 0:e.alt}),s.jsxs("div",{className:"flex flex-col items-start w-full ",children:[s.jsx("a",{className:"text-medium font-semibold",href:"",children:r}),s.jsx("p",{className:"text-small",children:t})]})]});l.__docgenInfo={description:"",methods:[],displayName:"Card",props:{title:{required:!0,tsType:{name:"string"},description:""},cover:{required:!1,tsType:{name:"signature",type:"object",raw:`{
-  src: string;
-  alt: string;
-}`,signature:{properties:[{key:"src",value:{name:"string",required:!0}},{key:"alt",value:{name:"string",required:!0}}]}},description:""},description:{required:!0,tsType:{name:"string"},description:""}}};export{l as C};
