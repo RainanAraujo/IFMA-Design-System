76eac04d89bfcc49bb4dbb952ff6fef2a5aececd
RainanAraujo <RainanAraujo@users.noreply.github.com>
2024-11-05 14:36:41 +0000

    deploy: 0cc2cb3f3276a8834b36cb86a6f7f150ea474a3a

---
+++ storybook/assets/InputField.stories-D6tlZcWA.js
@@ -0,0 +1,3 @@
+import{j as l}from"./jsx-runtime-j_jdvEMj.js";import{I as s}from"./InputField-B48p1609.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./cn-DHkRjjEP.js";import"./Input-BfFmV5y9.js";import"./Label-B4RhECvQ.js";const b={title:"Molecules/InputField",component:s,parameters:{layout:"centered"},argTypes:{className:{control:{disable:!0}}},args:{placeholder:"Enter text",label:"Label"}},e={render:r=>l.jsx(s,{...r,label:r.label})};var t,a,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: args => <InputField {...args} label={args.label} />
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,b as default};
