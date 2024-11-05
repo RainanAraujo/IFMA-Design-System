8bc92fa323a4c8848de454cfb57dda5a5da3699b
RainanAraujo <RainanAraujo@users.noreply.github.com>
2024-11-05 17:09:30 +0000

    deploy: 36bfb94d75b718fa4af473957305a547088e1c93

--- storybook/assets/IconButton.stories-1b9oJIWq.js
+++
@@ -1,9 +0,0 @@
-import{j as o}from"./jsx-runtime-j_jdvEMj.js";import{f as p}from"./index-YkVIqR4J.js";import{w as l}from"./Check-BQ_S7eJn.js";import{B as a}from"./Button-CjbFH56n.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./IconBase-BiRCNEL7.js";import"./Loading-Der4nm1y.js";import"./cn-DHkRjjEP.js";const I={title:"Molecules/IconButton",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{className:{control:{disable:!0}}},args:{onClick:p()}},t={render:e=>o.jsx(a,{...e,"aria-label":"Button Icon",children:o.jsx(l,{weight:"bold"})})},r={render:e=>o.jsxs(a,{...e,children:[o.jsx(l,{weight:"bold"})," Button"]})};var s,n,c;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  render: args => <Button {...args} aria-label="Button Icon">
-      <Check weight="bold" />
-    </Button>
-}`,...(c=(n=t.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var i,m,u;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  render: args => <Button {...args}>
-      <Check weight="bold" /> Button
-    </Button>
-}`,...(u=(m=r.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};const k=["Default","ButtonWithIcon"];export{r as ButtonWithIcon,t as Default,k as __namedExportsOrder,I as default};
