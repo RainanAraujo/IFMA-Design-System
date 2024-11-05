b3659594d18fc60f60ef452ad9dd9eb75a4daa49
RainanAraujo <RainanAraujo@users.noreply.github.com>
2024-11-05 17:03:35 +0000

    deploy: d37482f2c1ba4aafe77b4dd695defbfd8c371737

---
+++ storybook/assets/Header.stories-BCiX1Yuw.js
@@ -0,0 +1,36 @@
+import{j as e}from"./jsx-runtime-j_jdvEMj.js";import{B as a}from"./Button-CjbFH56n.js";import{L as r}from"./Link-RExFqD_a.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Loading-Der4nm1y.js";import"./IconBase-BiRCNEL7.js";import"./cn-DHkRjjEP.js";const p={title:"Organisms/Header",parameters:{layout:"fullscreen"}},n=()=>e.jsx("header",{className:"flex w-full  bg-gray-100 ",children:e.jsxs("div",{className:"flex items-center m-auto justify-between w-full max-w-7xl p-4 px-4 gap-6",children:[e.jsx("h1",{className:"text-xl",children:"Logo"}),e.jsx("nav",{children:e.jsxs("ul",{className:"flex gap-4 items-center",children:[e.jsx("li",{children:e.jsx(r,{href:"",className:"no-underline text-black hover:text-green-500",children:"Link"})}),e.jsx("li",{children:e.jsx(r,{href:"",className:"no-underline text-black hover:text-green-500",children:"Link"})}),e.jsx("li",{children:e.jsx(r,{href:"",className:"no-underline text-black hover:text-green-500",children:"Link"})}),e.jsx("li",{children:e.jsx(r,{href:"",className:"no-underline text-black hover:text-green-500",children:"Link"})}),e.jsx("li",{children:e.jsx(a,{variant:"outline",size:"small",children:"Sign Up"})})]})})]})});n.__docgenInfo={description:"",methods:[],displayName:"Header"};var l,i,t;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`() => {
+  return <header className="flex w-full  bg-gray-100 ">
+      <div className="flex items-center m-auto justify-between w-full max-w-7xl p-4 px-4 gap-6">
+        <h1 className="text-xl">Logo</h1>
+        <nav>
+          <ul className="flex gap-4 items-center">
+            <li>
+              <Link href="" className="no-underline text-black hover:text-green-500">
+                Link
+              </Link>
+            </li>
+            <li>
+              <Link href="" className="no-underline text-black hover:text-green-500">
+                Link
+              </Link>
+            </li>
+            <li>
+              <Link href="" className="no-underline text-black hover:text-green-500">
+                Link
+              </Link>
+            </li>
+            <li>
+              <Link href="" className="no-underline text-black hover:text-green-500">
+                Link
+              </Link>
+            </li>
+            <li>
+              <Button variant="outline" size="small">
+                Sign Up
+              </Button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>;
+}`,...(t=(i=n.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const f=["Header"];export{n as Header,f as __namedExportsOrder,p as default};
