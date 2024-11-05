7f830e48104d0c32dff7ddf4e1e8bccf76dcb2ac
RainanAraujo <RainanAraujo@users.noreply.github.com>
2024-11-05 16:53:56 +0000

    deploy: c64e605aff8b679c25a5d56e531a952338c370fe

---
+++ storybook/assets/Select.stories-CaMr2lcl.js
@@ -0,0 +1,12 @@
+import{j as l}from"./jsx-runtime-j_jdvEMj.js";import{S as r}from"./Select-BdtfjmbV.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./cn-DHkRjjEP.js";import"./IconBase-BiRCNEL7.js";import"./Input-BfFmV5y9.js";const f={title:"Atoms/Select",component:r,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{className:{control:!1},options:{control:!1}}},e={render:s=>l.jsx(r,{...s,className:"w-[280px]",options:[{value:"1",label:"Option 1"},{value:"2",label:"Option 2"},{value:"3",label:"Option 3"}]})};var a,t,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: args => <Select {...args} className="w-[280px]" options={[{
+    value: "1",
+    label: "Option 1"
+  }, {
+    value: "2",
+    label: "Option 2"
+  }, {
+    value: "3",
+    label: "Option 3"
+  }]} />
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,f as default};
